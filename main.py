2381d5988f0d77a07e8781e76e8f393aaeb74355
appscraftbd <appscraftbd@gmail.com>
2023-06-24 10:12:03 +0600

    version 0.1

---
+++ main.py
@@ -0,0 +1,19 @@
+data = [{
+    "Name": "Nahid Hossen",
+    "Roll": "669578",
+    "Semester": "3rd"
+},
+    {
+"Name": "Nahid Islam",
+    "Roll": "669578",
+    "Semester": "3rd"
+    }
+
+
+]
+
+data = data[1]
+data = data["Name"]
+print(data)
+
+
